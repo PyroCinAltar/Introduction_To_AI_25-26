a17c84a487ff982df86aad2b8377750a494ef4ab
Nicholas <lynnhilife@gmail.com>
2025-09-17 09:40:58 -0400

    Algorithm Practice

---
+++ Leoons01_Algorithm_Basics/sandwich._run.py
@@ -0,0 +1,28 @@
+# print("Step 1: Get two slices of bread")
+# print("Step 2: Spread peanut butter")
+# print("Step 3: Add jelly")
+# print("Step 4: Place one slice of bread of the the other slice with peanut butter and jelly.")
+
+
+
+# age = int(input("Age: "))
+# isEligible = False
+
+# if age >= 18:
+#     isEligible = True
+#     print(f"Is Eligible: {isEligible}")
+# else: 
+#     print(f"Is Eligible: {isEligible}")
+    
+# for i in range(5):
+#     print("This is step", i+1)
+
+
+input_number = int(input("Favorite number: "))
+
+if input_number % 2 == 1:
+    print(f"{input_number} is odd")
+elif input_number % 2 == 0:
+    print(f"{input_number} is even")
+else:
+    print("Please enter a valid number")
